ea96815ee3abafa87a6681a0d0a525e632834109
Wwwsylvia <lixia_lei@outlook.com>
2016-07-14 17:01:06 +0800

    added show-QR-code

--- static/js/check-in-meetings.js
+++ static/js/check-in-meetings.js
@@ -1,3 +1,38 @@
 /**
  * Created by m on 2016/7/14.
  */
+window.onload = showRecentMeetings();
+
+
+function showRecentMeetings() {
+    $('body').html("");
+
+    $.ajax({
+        url: '/meeting/',
+        method: 'GET',
+        dataType: 'JSON',
+        success: function (response) {
+            console.log(response);
+
+            if (response.result === "ok") {
+                var length = response.meetings.length;
+                for (var i = 0; i < length; i++) {
+                    $('body').append(
+                        "<div class='meeting-item'>"
+                        + "<h2 class='meeting-name'>"
+                        + response.meetings[i].title
+                        + "</h2>"
+                        + "<span class='meeting-location'>"
+                        + response.meetings[i].room
+                        + "</span>"
+                        + "<span class='meeting-time'>"
+                        + response.meetings[i].time
+                        + "</span>"
+                        + "</div>"
+                    );
+                }
+            }
+
+        }
+    });
+}
