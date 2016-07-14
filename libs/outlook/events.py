1cbc5d96a19a9847ba847e33e84dbb53e844684f
Zihang Chen <chsc4698@gmail.com>
2016-07-14 10:35:51 +0800

    qwer

--- libs/outlook/events.py
+++ libs/outlook/events.py
@@ -1,43 +1,37 @@
 from collections import OrderedDict
 from pprint import pprint
-from urllib.parse import urljoin
-from urllib.request import Request, urlopen
 
 import requests
 from pymongo import ReturnDocument
 
 from app import mongo
-from libs.constants import GRAPH_EP
 from libs.oauth import get_token
+from libs.outlook import graph
 
 
 def get_events_by_wxid(u):
-    headers = OrderedDict([
-        # ('User-Agent', 'bxchidao/0.1.0'),
-        ('Authorization', 'Bearer %s' % u['outlook']['token']),
-        ('Accept', 'application/json'),
-        ('Content-Type', 'application/json')
-    ])
+    headers = {
+        'User-Agent': 'bxchidao/0.1.0',
+        'Authorization': 'Bearer %s' % u['outlook']['token'],
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
 
-    url = 'me/events'
-    url = '/'.join((GRAPH_EP, url))
-    # req = Request(url, headers=headers)
-    r = requests.get('/'.join((GRAPH_EP, url)),
+    r = requests.get(graph('/me/events'),
                      headers=headers)
-    print(r.url)
-    return r
+
+    return r.json()
 
 
-def get_events_by_wxid_x(mongo, openid):
+def get_events_by_wxid_x(openid):
     u = mongo.users.find_one({'openid': openid})
 
     r = get_events_by_wxid(u)
+    pprint(r)
 
-    print(r)
     if 'odata.error' in r:
-        if True: # r['odata.error']['code'] == 'Authentication_ExpiredToken':
+        if r['odata.error']['code'] == 'Authentication_ExpiredToken':
             r = get_token('refresh_token', u['outlook']['refresh_token'])
-            pprint(r)
 
             doc = mongo.users.find_one_and_update({
                 'openid': openid
@@ -48,11 +42,8 @@ def get_events_by_wxid_x(mongo, openid):
                     'outlook.token': r['access_token']
                 }
             }, return_document=ReturnDocument.AFTER)
-            pprint(doc)
 
-            r = get_events_by_wxid(doc)
-            pprint(r)
-            return r
+            return get_events_by_wxid(doc)
     else:
         return r
 
